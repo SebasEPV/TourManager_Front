5649c672ab0b9ba1886e96f28c1722369b297268
KristalPalma <krisprogra@gmail.com>
2025-02-21 08:19:03 -0500

    RUTAS CORREGIDAS

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Header from "./layout/header";
-import Login from "./pages/login";
-import Register from "./pages/register";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 import PaymentManagement from "./pages/PaymentManagement";
 import TourManagement from "./pages/TourManagement";
 import ReservationManagement from "./pages/ReservationManagement";
@@ -34,7 +34,7 @@ function App() {
       <Header />
       <Routes>
         {/* PUBLIC ROUTES */}
-        <Route path="/" element={<Home />} />
+        {/* <Route path="/" element={<Home />} /> */}
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/mfa" element={<MFAPage />} />
