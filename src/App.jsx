8157dd4fed3844ad8ce15b03b2f0c4be089e6526
SebasEPV <sperezvinas@gmaill.com>
2025-02-23 17:38:39 -0500

    Updated Login Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/home";
 import Header from "./layout/header";
-import Login from "./pages/login";
+import Login from "./pages/Login";
 import Register from "./pages/Register";
 import PaymentManagement from "./pages/PaymentManagement";
 import TourManagement from "./pages/TourManagement";
